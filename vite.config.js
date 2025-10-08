8d2ae91b66941bbdd7a92a7918e5b11957471882
starnovawars <warsstarnova@gmail.com>
2025-10-08 17:46:06 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cyberglow-swap/', // 👈 cambia esto si tu repo tiene otro nombre
+  base: '/cyberglow-swap/', // 👈 Debe coincidir EXACTO con tu repo
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 })
