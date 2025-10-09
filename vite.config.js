1bef970ccf497d7dcbad8991f0aab63d295100e4
Jarrs89 <warsstarnova@gmail.com>
2025-10-09 06:17:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cyberglow-swap/', // 👈 Coincide con el nombre del repo
+  base: '/cyberglow-swap/',
   build: {
-    outDir: 'dist',
-  },
+    outDir: 'dist'
+  }
 })
